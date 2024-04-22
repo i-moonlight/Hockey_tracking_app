9109cd0ff809b39f5f2de5fee7a3ba2927d6e98d
i-moonlight <infinite.moonlight0506@gmail.com>
2024-04-22 05:50:50 -0400

    Hocky UI Test

---
+++ HockyUITests/HockyUITestsLaunchTests.m
@@ -0,0 +1,37 @@
+//
+//  HockyUITestsLaunchTests.m
+//  HockyUITests
+//
+//  Created by milt on 12/14/23.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HockyUITestsLaunchTests : XCTestCase
+
+@end
+
+@implementation HockyUITestsLaunchTests
+
++ (BOOL)runsForEachTargetApplicationUIConfiguration {
+    return YES;
+}
+
+- (void)setUp {
+    self.continueAfterFailure = NO;
+}
+
+- (void)testLaunch {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app launch];
+
+    // Insert steps here to perform after app launch but before taking a screenshot,
+    // such as logging into a test account or navigating somewhere in the app
+
+    XCTAttachment *attachment = [XCTAttachment attachmentWithScreenshot:XCUIScreen.mainScreen.screenshot];
+    attachment.name = @"Launch Screen";
+    attachment.lifetime = XCTAttachmentLifetimeKeepAlways;
+    [self addAttachment:attachment];
+}
+
+@end
